e52977d7978ae9be4fe25e21c5aa6a49db362509
KenzieJeremyG <104441957+KenzieJeremyG@users.noreply.github.com>
2023-03-20 16:04:14 -0700

    fixed DynamoDBGameRepository and started on possible Lobby and Room classes

--- Application/src/main/java/com/kenzie/appserver/service/GameService.java
+++ Application/src/main/java/com/kenzie/appserver/service/GameService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public class GameService {
     private GameRepository gameRepository;
 
-    @Autowired
+
     public GameService(GameRepository gameRepository) {
         this.gameRepository = gameRepository;
     }
